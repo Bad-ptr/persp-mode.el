ca984088ac9742aea92dd0988f10cbdeab7220af
Constantin Kulikov <zxnotdead@gmail.com>
2017-11-04 20:28:49 +0300

    More flexible major-mode restoration of a saved buffer:
    If the buffer has the `persp-load-buffer-mode-restore-function' buffer-local
    variable, then call function stored in this var instead of the
    (symbol-function major-mode).
    
    Add mode-restore-function keyword parameter to the persp-def-buffer-save/load.

--- persp-mode.el
+++ persp-mode.el
@@ -1646,6 +1646,7 @@ the selected window to a wrong buffer.")
      keyargs
      &key buffer-name file-name mode mode-name minor-mode minor-mode-name
      predicate tag-symbol save-vars save-function load-function after-load-function
+     mode-restore-function
      append)
   (let ((generated-save-predicate
          (apply #'persp--generate-buffer-predicate keyargs))
@@ -1687,6 +1688,10 @@ the selected window to a wrong buffer.")
                  (buffer-name vars-list &rest _rest) (cdr savelist)
                (let ((buf-file (alist-get 'buffer-file-name vars-list))
                      (buf-mmode (alist-get 'major-mode vars-list)))
+                 ,(when mode-restore-function
+                    `(push (cons 'persp-load-buffer-mode-restore-function
+                                 (with-no-warnings ',mode-restore-function))
+                           vars-list))
                  (lexical-let
                      ((persp-loaded-buffer
                        (persp-buffer-from-savelist
@@ -3894,12 +3899,17 @@ of the perspective %s can't be saved."
                             (alist-get 'local-vars parameters))))
                       (with-current-buffer buf
                         (restorevars)
-                        (typecase mode
-                          (function
-                           (when (and (not (eq major-mode mode))
-                                      (not (eq major-mode 'not-loaded-yet)))
-                             (funcall mode)
-                             (restorevars)))))))
+                        (cond
+                         ((and (boundp 'persp-load-buffer-mode-restore-function)
+                               (variable-binding-locus 'persp-load-buffer-mode-restore-function)
+                               (functionp persp-load-buffer-mode-restore-function))
+                          (funcall persp-load-buffer-mode-restore-function mode)
+                          (restorevars))
+                         ((functionp mode)
+                          (when (and (not (eq major-mode mode))
+                                     (not (eq major-mode 'not-loaded-yet)))
+                            (funcall mode)
+                            (restorevars)))))))
                   buf))))
       (persp-car-as-fun-cdr-as-args savelist))))
 
