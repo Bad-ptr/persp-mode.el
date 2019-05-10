Bad-ptr/persp-mode.el

commit b476af23e15c3c04072f9816afb686184849e177

Fix persp-copy.

persp-mode.el
@@ -2213,14 +2213,13 @@ Return removed buffers."
                                (append (persp-buffers current-persp) nil)
                              (safe-persp-buffers current-persp)))))
            (new-persp (persp-add-new new-name)))
-      (when (listp choosen-buffers)
-        (setq new-buffers (mapcar #'get-buffer choosen-buffers)))
       (when new-persp
         (persp-save-state current-persp)
-        (setf (persp-buffers new-persp) new-buffers
-              (persp-window-conf new-persp) (safe-persp-window-conf current-persp)
+        (setf (persp-window-conf new-persp) (safe-persp-window-conf current-persp)
               (persp-parameters new-persp) (append (safe-persp-parameters current-persp) nil)
               (persp-weak new-persp) (if current-persp (persp-weak current-persp) nil))
+        (when (listp choosen-buffers)
+          (persp-add-buffer choosen-buffers new-persp nil nil))
         (case switch
           (window (persp-window-switch new-name))
           (frame (persp-frame-switch new-name))

commit 55aa8a8854cb0f9e8d5bcc91e787fb38628f0646

persp-copy: Fix adding buffers when not choosing
Fix persp-copy to add buffers to the new perspective even when the list of
buffers is not interactively chosen.

Commit b476af23e15c3c04072f9816afb686184849e177 broke this logic.

* persp-mode.el (persp-copy): Add new-buffers to the perspective if
choosen-buffers is not a list.

persp-mode.el
@@ -2626,8 +2626,11 @@ cause it already contain all buffers.")))
               (copy-list (safe-persp-parameters current-persp))
               (persp-weak new-persp)
               (if current-persp (persp-weak current-persp) nil))
-        (when (listp choosen-buffers)
-          (persp-add-buffer choosen-buffers new-persp nil nil))
+        (let ((buffers (if (listp choosen-buffers)
+                           choosen-buffers
+                         new-buffers)))
+          (when buffers
+            (persp-add-buffer buffers new-persp nil nil)))
         (case switch
           (window (persp-window-switch new-name))
           (frame (persp-frame-switch new-name))