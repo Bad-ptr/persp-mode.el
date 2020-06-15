6af919f1cc838fda24edfc5a603b82bb325f52be
Constantin Kulikov <zxnotdead@gmail.com>
2020-06-15 15:10:37 +0300

    Up version.

--- persp-mode.el
+++ persp-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2012 Constantin Kulikov
 
 ;; Author: Constantin Kulikov (Bad_ptr) <zxnotdead@gmail.com>
-;; Version: 2.9.8
+;; Version: 3.0.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; Keywords: perspectives, session, workspace, persistence, windows, buffers, convenience
 ;; URL: https://github.com/Bad-ptr/persp-mode.el
